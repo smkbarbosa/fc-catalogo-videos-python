da8ff82369ab7e39dee2adcda581be3fa05a6dd5
Samuel Barbosa <contato@smkbarbosa.eti.br>
2023-03-01 04:04:13 +0000

    desafio repositorio e validação da categoria

--- src/category/infra/repositories.py
+++ src/category/infra/repositories.py
@@ -1,6 +1,22 @@
-from __seedwork.domain.repositories import InMemoryRepository
+from typing import List
+from __seedwork.domain.repositories import InMemorySearchableRepository
+from category.domain.entities import Category
 from category.domain.repositories import CategoryRepository
 
 
-class CategoryInMemoryRepository(CategoryRepository, InMemoryRepository):
-    pass
+class CategoryInMemoryRepository(CategoryRepository, InMemorySearchableRepository):
+    # Poderemos filtrar categorias por seu nome, qualquer termo passado deverá ser pesquisa 
+    # do começo ao fim do valor do nome
+    # Se no filtro não existir um parâmetro de ordenação, precisa ordenar por padrão pelo created_at.
+    sortable_fields: List[str] = ["name", "created_at"]
+
+    def _apply_filter(self, items: List[Category], filter_param: str = None) -> List[Category]:
+        if filter_param:
+            obj = filter(
+                lambda x: x.name.lower() in filter_param.lower(), items
+            )
+            return list(obj)
+        return items
+    
+    def _apply_sort(self, items: List[Category], sort: str = None, sort_dir: str = None) -> List[Category]:
+        return super()._apply_sort(items, sort, sort_dir) if sort else super()._apply_sort(items, 'created_at', 'desc')
